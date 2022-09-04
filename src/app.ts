9330c2462d95debac0aa142e113228819a376477
Pawel  Kurek <pawel.kurek975@gmail.com>
2022-09-04 21:17:41 +0200

    supply matches with id's

--- src/app.ts
+++ src/app.ts
@@ -16,15 +16,22 @@ function searchForPhrase() {
     // Include regex flags
     let searchFlags: string = "";
     if (ignoreCaseFlag.checked) searchFlags += "i";
-    if (globalSearchFlag.checked) searchFlags += "g";
+    if (!globalSearchFlag.checked) searchFlags += "g";
 
     let searched: string = searchedText.value.trim();
     if (searched !== "") {
 
         let text: string = textToScanContainer.innerHTML;
         let re = new RegExp(searched, searchFlags);
+
+        let matchIndex = 0;
+
         let newText: string = text.replace
-        (re, (match) => (`<mark class="highlighted-text">${match}</mark>`));
+        (re, (match) => {
+            const highlightedText = `<mark id="${matchIndex}" class="highlighted-text">${match}</mark>`
+            matchIndex++
+            return highlightedText;
+        });
         textToScanContainer.innerHTML = newText;
     }
 }
@@ -32,7 +39,7 @@ function searchForPhrase() {
 // Cleans up all highlights from text
 function resetHighlights() {
     let text: string = textToScanContainer.innerHTML;
-    let highlightTag = new RegExp(`<mark class="highlighted-text">`,"g");
+    let highlightTag = new RegExp(`\<(.*?)\>`,"g");
     let scannedText: string = text.replace(highlightTag, "");
     textToScanContainer.innerHTML = scannedText;
     
