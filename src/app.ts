a884d9a9fa43a3c4a937bfe2b8366fdfb6582264
Pawel  Kurek <pawel.kurek975@gmail.com>
2022-09-04 22:00:15 +0200

    clicking through matches

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,32 @@
 import { domElements } from "./domElements";
 import "./styles.css";
 
-const {searchForm, searchedText, textToScanContainer, ignoreCaseFlag, globalSearchFlag} = domElements;
+const {searchForm, searchedText, textToScanContainer, ignoreCaseFlag, globalSearchFlag, resultChooserMatchesCount, resultChooserNextMatchBtn, resultChooserPrevMatchBtn} = domElements;
+
+let matchIndex: number = 0;
+let choosedMatchIndex: number = 0;
 
 searchForm.addEventListener("submit", (e) => {
     e.preventDefault();
     searchForPhrase();
 })
 
+resultChooserPrevMatchBtn.addEventListener("click", () => {
+    if (choosedMatchIndex > 0) choosedMatchIndex--;
+    resultChooserMatchesCount.textContent = `${choosedMatchIndex}/${matchIndex}`;
+    chooseHighlight();
+})
+
+resultChooserNextMatchBtn.addEventListener("click", () => {
+    if (choosedMatchIndex < matchIndex) choosedMatchIndex++;
+    resultChooserMatchesCount.textContent = `${choosedMatchIndex}/${matchIndex}`;
+    chooseHighlight();
+})
+
 function searchForPhrase() {
 
     resetHighlights();
+    matchIndex = 0;
 
     // Include regex flags
     let searchFlags: string = "";
@@ -24,23 +40,38 @@ function searchForPhrase() {
         let text: string = textToScanContainer.innerHTML;
         let re = new RegExp(searched, searchFlags);
 
-        let matchIndex = 0;
 
-        let newText: string = text.replace
-        (re, (match) => {
+        let newText: string = 
+        text.replace(re, (match): string => {
             const highlightedText = `<mark id="${matchIndex}" class="highlighted-text">${match}</mark>`
             matchIndex++
             return highlightedText;
         });
+
         textToScanContainer.innerHTML = newText;
+        resultChooserMatchesCount.textContent = `${choosedMatchIndex}/${matchIndex}`;
+
     }
+
+    chooseHighlight();
 }
 
 // Cleans up all highlights from text
 function resetHighlights() {
     let text: string = textToScanContainer.innerHTML;
+    // Search for tags and erase them
     let highlightTag = new RegExp(`\<(.*?)\>`,"g");
     let scannedText: string = text.replace(highlightTag, "");
     textToScanContainer.innerHTML = scannedText;
-    
+}
+
+function chooseHighlight() {
+    const allHighlights = document.getElementsByTagName("mark");
+    Array.from(allHighlights).forEach(highlight => {
+        highlight.classList.remove("choosed-highlighted-text");
+    })
+
+    const choosedHighlight = document.getElementById(String(choosedMatchIndex));
+    console.log(choosedHighlight)
+    choosedHighlight.classList.add("choosed-highlighted-text");
 }
--- src/domElements.ts
+++ src/domElements.ts
@@ -5,4 +5,7 @@ export const domElements = {
     textToScanContainer: <HTMLTextAreaElement> document.querySelector(".text-to-scan"),
     ignoreCaseFlag: <HTMLInputElement> document.getElementById("ignore-case"),
     globalSearchFlag: <HTMLInputElement> document.getElementById("global-search"),
+    resultChooserPrevMatchBtn: <HTMLButtonElement> document.querySelector(".result-chooser__prev"),
+    resultChooserNextMatchBtn: <HTMLButtonElement> document.querySelector(".result-chooser__next"),
+    resultChooserMatchesCount: <HTMLButtonElement> document.querySelector(".result-chooser__matches-count"),
 }
